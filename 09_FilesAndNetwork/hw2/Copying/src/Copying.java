d9200aa5bb1a93679697464114e6ee7f6b701d74
vftens <aico@ya.ru>
2020-06-27 21:13:11 +0300

    работает Copying
    работает Copying. To change to better behavior

--- 09_FilesAndNetwork/hw2/Copying/src/Copying.java
+++ 09_FilesAndNetwork/hw2/Copying/src/Copying.java
@@ -2,22 +2,45 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
+
+import static java.nio.file.Files.createDirectory;
 
 public class Copying {
 
     public static void main(String[] args) throws InterruptedException, IOException {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
 
-            System.out.println("Введите путь до папки: ");
+            System.out.println("Введите путь до исходной папки А :");
             Path path = Paths.get(reader.readLine());
+            System.out.println("Введите путь до конечной папки Б");
+            Path pathB = Paths.get(reader.readLine());
 
+            FileVizitor fileVizitor = new FileVizitor();
+            if (!path.toFile().isDirectory()) {
+                System.out.println("ИЗВИНИТЕ, " + path.toAbsolutePath() + " - не папка");
+            } else {
+                if (!pathB.toFile().isDirectory()) {
+                    createDirectory(pathB);  // создаем отсутствующую директорию
+                    //System.out.println("ИЗВИНИТЕ, " + path.toAbsolutePath() + " - не папка");
+                }
+                long sizeInByte;
+
+                Files.walkFileTree(path, fileVizitor);
+                System.out.println("Всего папок - " + (fileVizitor.getFoldersCount() - 1));
+                System.out.println("Всего файлов - " + fileVizitor.getFilesCount());
+
+                sizeInByte = fileVizitor.getSize();
+                System.out.println("Общий размер - " + sizeInByte + " байт");
+
+
+            }
+
+            /*
             ArrayList<String> selectFiles = new ArrayList<>();
-            File folder = new File("путь");
+            File folder = new File(String.valueOf(pathB));
             File[] listOfFiles = folder.listFiles();
 
             for (File f : listOfFiles) {
@@ -25,15 +48,48 @@ public class Copying {
             }
 
             File source = new File(" " + selectFiles);// ПОЛУЧАЮ СПИСОК ФАЙЛОВ ПРИСВАИВАЮ ПЕРЕМЕННУЮ С ФАЙЛАМИ
-            File dest = new File("путь");// ПРОПИСЫВАЮ ПУТЬ КУДА КОПИРОВАТЬ
-            copy(source, dest);
-        }catch (Exception ex){
+            File dest = new File(String.valueOf(pathB));// ПРОПИСЫВАЮ ПУТЬ КУДА КОПИРОВАТЬ
+
+             */
+            copyDir(String.valueOf(path), String.valueOf(pathB)); // dest;
+        } catch (Exception ex) {
             System.out.println(ex);
+            ex.printStackTrace();
         }
     }
 
     public static void copy(File source, File dest) throws IOException {
         Files.copy(source.toPath(), dest.toPath());
+    }
+
+    static class FileVizitor extends SimpleFileVisitor<Path> {
+        private int foldersCount = 0;
+        private int filesCount = 0;
+        private long size = 0;
+
+        public int getFilesCount() {
+            return filesCount;
+        }
+
+        public int getFoldersCount() {
+            return foldersCount;
+        }
+
+        public long getSize() {
+            return size;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+
+            return super.visitFile(file, attrs);
+        }
+
+        @Override
+        public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+            ++foldersCount;
+            return super.postVisitDirectory(dir, exc);
+        }
     }
 
     private static void copyDir(String sourceDirName, String targetSourceDir) throws IOException {
--- 09_FilesAndNetwork/hw4/Jsouping/src/main/java/Jsoupin.java
+++ 09_FilesAndNetwork/hw4/Jsouping/src/main/java/Jsoupin.java
@@ -1,3 +1,5 @@
+import org.jsoup.Jsoup;
+
 import java.sql.Connection;
 
 public class Jsoupin {
