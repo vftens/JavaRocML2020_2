f825b29444d2b181ae219a44beda88829aec23d9
vftens <aico@ya.ru>
2020-06-30 14:14:36 +0300

    Exception VKCore
    Exception VKCore

---
+++ 09_FilesAndNetwork/hw4 Бот для ВКонтакте/src/main/java/vk/VKServer.java
@@ -0,0 +1,47 @@
+package vk;
+
+
+import com.vk.api.sdk.exceptions.ApiException;
+import com.vk.api.sdk.exceptions.ClientException;
+import com.vk.api.sdk.objects.messages.Message;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class VKServer {
+    public static VKCore vkCore;
+
+    static {
+        try {
+            vkCore = new VKCore();
+        } catch (ApiException | ClientException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws NullPointerException, ApiException, InterruptedException, IOException {
+
+
+        System.out.println("Running server...");
+        jsouping.Images.mymain("");  // call of my Jsouping Download Images app
+
+        while (true) {
+            Thread.sleep(300);
+
+            try {
+                Message message = vkCore.getMessage();
+                if (message != null) {
+                    ExecutorService exec = Executors.newCachedThreadPool();
+                    exec.execute(new Messenger(message));
+                }
+            } catch (ClientException e) {
+                System.out.println("Возникли проблемы");
+                final int RECONNECT_TIME = 10000;
+                System.out.println("Повторное соединение через " + RECONNECT_TIME / 1000 + " секунд");
+                Thread.sleep(RECONNECT_TIME);
+
+            }
+        }
+    }
+}
