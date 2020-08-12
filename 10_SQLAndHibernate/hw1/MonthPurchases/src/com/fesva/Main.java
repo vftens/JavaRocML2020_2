e57065d758c10e7a02b6a4615a0bf678aa421e02
vftens <aico@ya.ru>
2020-08-12 22:00:55 +0300

    SQL sending
    SQL sending  - how to solve the numeration task of DB

--- 10_SQLAndHibernate/hw1/MonthPurchases/src/com/fesva/Main.java
+++ 10_SQLAndHibernate/hw1/MonthPurchases/src/com/fesva/Main.java
@@ -23,27 +23,39 @@ public class Main {
                     "GROUP BY c.name  " +
                     "ORDER BY c.name";
 
-            /*
-            String SQLstr2 = "SELECT c.subscription_date AS 'Course name'," +
-                    "COUNT(sub.subscription_date)/12 AS 'AVG buy course per month'  " +
-                    "FROM Courses c " +
-                    "JOIN Subscriptions sub ON c.id=sub.course_id " +
-                    "GROUP BY c.name  " +
-                    "ORDER BY c.name";
+            // SELECT pl.course_name, pl.subscription_date FROM PurchaseList pl
+            // WHERE pl.course_name = "Веб-разработчик c 0 до PRO" ORDER BY pl.subscription_date;
 
-             */
+            String SQLstr2 =
+                    "SELECT c.name AS 'Course name', sub.subscription_date AS 'Subscr date', " +
+                            "COUNT(sub.subscription_date) AS 'AVG buy course per month'  " +
+                            "FROM Courses c " +
+                            //"WHERE c.course_name = c.name " +
+                            "JOIN Subscriptions sub ON c.id=sub.course_id " +
+                            "GROUP BY c.name  " +
+                            "ORDER BY c.name";
+
+            //*/
             ResultSet resultSet =
                     statement.executeQuery(SQLstr);
             //ResultSet resultSet2 =
-                    //statement.executeQuery(SQLstr2);
+            //statement.executeQuery(SQLstr2);
             System.out.println("Название курса\t\tСреднее количество покупок в месяц");
 
             while (resultSet.next()) {
                 String nameString = resultSet.getString("Course name");
                 String monthSubString = resultSet.getString("AVG buy course per month");
-                //if(resultSet2)
-                //String nameString2 = resultSet2.getString("Course name");
+
                 System.out.printf("%s \t\t\t-\t %s\n", nameString, monthSubString);
+            }
+
+            System.out.println();
+            ResultSet resultSet2 =
+                    statement.executeQuery(SQLstr2);
+            while (resultSet2.next()){
+                String nameString2 = resultSet2.getString("Course name");
+                String subscriptionDate = resultSet2.getString("Subscr date");
+                System.out.printf("%s \t %s\n", nameString2, subscriptionDate);
             }
         } catch (Exception ex) {
             ex.getStackTrace();
