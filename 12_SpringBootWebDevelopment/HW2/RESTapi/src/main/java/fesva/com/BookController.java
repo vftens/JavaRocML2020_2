d60f8842948e73a9632cc586b6963dd35029e209
vftens <aico@ya.ru>
2021-03-04 15:20:17 +0300

    changes
    changes small

--- 12_SpringBootWebDevelopment/HW2/RESTapi/src/main/java/fesva/com/BookController.java
+++ 12_SpringBootWebDevelopment/HW2/RESTapi/src/main/java/fesva/com/BookController.java
@@ -1,6 +1,5 @@
 package fesva.com;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,19 +14,9 @@ import static fesva.com.Storage.*;
 @RestController
 public class BookController {
 
-
-//    public List<Book> list() {
-  //      return Storage.getAllBooks();
-    //}
-
-    //@Controller
-    //public int add(Book book) {
-
-
     @RequestMapping(value = "/books/", method = RequestMethod.GET)
     public List<Book> list() {
-        return getAllBooks();
-
+        return Storage.getAllBooks();
     }
 
     @RequestMapping(value = "/books/", method = RequestMethod.POST)
--- 12_SpringBootWebDevelopment/HW2/RESTapi/src/main/java/fesva/com/Storage.java
+++ 12_SpringBootWebDevelopment/HW2/RESTapi/src/main/java/fesva/com/Storage.java
@@ -11,14 +11,9 @@ public class Storage {
     private static ArrayList<Book> books=new ArrayList<Book>();
 
     public static List<Book> getAllBooks() {
-        return null;  // ВОЗВРАЩАЕТ КОЛ-ВО КНИГ?
+        List<Book> books = Storage.books;
+        return books;  // ВОЗВРАЩАЕТ КОЛ-ВО КНИГ???
     }
-
-
-    //public void addBook(Book book){
-      //  books.add(book);
-    //}
-
 
     public static int addBook(Book book) {
         int id = books.size() + 1;
@@ -27,23 +22,4 @@ public class Storage {
 
         return id;
     }
-
-
-    /*
-    //@Override
-    public static List<Book> getAllBooks()
-    {
-        return books;
-    }
-
-    public static int addBook(Book book){
-        int id = books.size() + 1;
-        book.setId(id);
-        books.add(book);
-
-        return id;
-
-    }
-
-     */
 }
