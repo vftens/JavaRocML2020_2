b6ae3852617f0399d43c911bc4d4b2b7684d3bca
vftens <aico@ya.ru>
2021-03-06 18:34:04 +0300

    RedirectToIndex
    RedirectToIndex

--- 12_SpringBootWebDevelopment/HW2/RESTapi/src/main/java/fesva/com/DefaultController.java
+++ 12_SpringBootWebDevelopment/HW2/RESTapi/src/main/java/fesva/com/DefaultController.java
@@ -1,6 +1,7 @@
 package fesva.com;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
@@ -12,5 +13,11 @@ public class DefaultController {
     public String index()
     {
         return (new Date()).toString();
+    }
+
+    @RequestMapping(method = {RequestMethod.OPTIONS,
+            RequestMethod.GET}, value = "/**/{path:[^\\\\.]*}")
+    public String redirectToIndex() {
+        return "forward:/";
     }
 }
