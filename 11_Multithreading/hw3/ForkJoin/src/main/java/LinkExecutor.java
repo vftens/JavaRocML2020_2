45718eb0cf225b04698718413f636b7511cc85f4
vftens <aico@ya.ru>
2021-04-01 09:54:15 +0300

    Mypy
    Mypy

--- 11_Multithreading/hw3/ForkJoin/src/main/java/LinkExecutor.java
+++ 11_Multithreading/hw3/ForkJoin/src/main/java/LinkExecutor.java
@@ -3,60 +3,61 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.concurrent.RecursiveTask;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class LinkExecutor extends RecursiveTask<String> {
-  static final boolean debug = true;
+    static final boolean debug = true;
 
-  private String url;
-  private static String startUrl;
-  private static CopyOnWriteArraySet<String> allLinks = new CopyOnWriteArraySet<>();
+    private String url;
+    private static String startUrl;
+    private static CopyOnWriteArraySet<String> allLinks = new CopyOnWriteArraySet<>();
 
-  public LinkExecutor(String url) {
-    this.url = url.trim();
-  }
-
-  public LinkExecutor(String url, String startUrl) {
-    this.url = url.trim();
-    LinkExecutor.startUrl = startUrl.trim();
-  }
-
-
-  @Override
-  protected String compute() {
-    StringBuffer sb = new StringBuffer(url + "\n");
-    Set<LinkExecutor> subTask = new HashSet<>();
-
-    getChildren(subTask);
-
-    for (LinkExecutor link : subTask) {
-      sb.append(link.join());
+    public LinkExecutor(String url) {
+        this.url = url.trim();
     }
-    return sb.toString();
-  }
 
-  private void getChildren(Set<LinkExecutor> subTask) {
-    Document doc;
-    Elements elements;
-    try {
-      Thread.sleep(SiteMap.pause());
-      if (debug) System.out.println(url);
-      doc = Jsoup.connect(url).get();
-      elements = doc.select("a");
-      for (Element el : elements) {
-        String attr = el.attr("abs:href");
-        if (!attr.isEmpty() && attr.startsWith(startUrl) && !allLinks.contains(attr) && !attr
-            .contains("#")) {
-          LinkExecutor linkExecutor = new LinkExecutor(attr);
-          linkExecutor.fork();
-          subTask.add(linkExecutor);
-          allLinks.add(attr);
+    public LinkExecutor(String url, String startUrl) {
+        this.url = url.trim();
+        LinkExecutor.startUrl = startUrl.trim();
+    }
+
+
+    @Override
+    protected String compute() {
+        StringBuffer sb = new StringBuffer(url + "\n");
+        Set<LinkExecutor> subTask = new HashSet<>();
+
+        getChildren(subTask);
+
+        for (LinkExecutor link : subTask) {
+            sb.append(link.join());
         }
-      }
-    } catch (InterruptedException | IOException ignored) {
+        return sb.toString();
     }
-  }
+
+    private void getChildren(Set<LinkExecutor> subTask) {
+        Document doc;
+        Elements elements;
+        try {
+            Thread.sleep(SiteMap.pause());
+            if (debug) System.out.println(url);
+            doc = Jsoup.connect(url).get();
+            elements = doc.select("a");
+            for (Element el : elements) {
+                String attr = el.attr("abs:href");
+                if (!attr.isEmpty() && attr.startsWith(startUrl) && !allLinks.contains(attr) && !attr
+                        .contains("#")) {
+                    LinkExecutor linkExecutor = new LinkExecutor(attr);
+                    linkExecutor.fork();
+                    subTask.add(linkExecutor);
+                    allLinks.add(attr);
+                }
+            }
+        } catch (InterruptedException | IOException ignored) {
+        }
+    }
 }
