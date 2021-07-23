a585c1af39356b045672a748fd742c40c04dfa72
vftens <aico@ya.ru>
2021-07-23 21:41:35 +0300

    MyApplicationWebView running
    MyApplicationWebView running. In 12  SpringBootWebDevelopment Java

---
+++ 12_SpringBootWebDevelopment/MyApplicationWebView/app/src/main/java/com/fesva/myapplicationwebview/MainActivity.java
@@ -0,0 +1,41 @@
+package com.fesva.myapplicationwebview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = findViewById(R.id.webView);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.afuturevalue.com/");
+
+        WebViewClient webViewClient = new WebViewClient() {
+            @SuppressWarnings("deprecation")
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            @TargetApi(Build.VERSION_CODES.N)
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                view.loadUrl(request.getUrl().toString());
+                return true;
+            }
+
+        };
+        webView.setWebViewClient(webViewClient);
+    }
+}
